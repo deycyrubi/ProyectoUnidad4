6ff214e0308ec297aac8ab2adb4776e5e62a6606
DaycyPM <Deycy5julio@gmail.com>
2019-05-18 20:39:50 -0500

    Este es el proyecto para evaluar la unidad 4 de la materia de POO

--- JuegoAdivinaPar.py
+++ JuegoAdivinaPar.py
@@ -4,35 +4,27 @@ from random import*
 class JuegoAdivinaPar:
 #Se identifico el nombre de la clase
 #Creamos la clase de nuestro nuevo juego que consiste en adivinar si el numero es par o impar
-      __numeroPar = int(0)
-      __numeroImpar = int(0)
-      __validaNumero = int(0)
-"""Hemos declarado nuestros atributos"""
+    __NumeroPar = int(0)
+    __NumeroImpar = int(0)
+    __ValidaNumero =int(0)
+    __a = int(0)
+    """Hemos declarado nuestros atributos"""
 
-def __init__(self): #Se identifico el metodo constructor
-    self.__numeroPar
+    def __init__(self): #Se identifico el metodo constructor
+        self.__NumeroPar
+        self.__NumeroImpar
 
+    def validaNumero(self):
+        self.__ValidaNumero
+        return 0
 
-def numeroPar(self):
-    """Se declaro el metodo"""
-    self.__numeroPar
-    return 0
+    a = 0
+    a = int(input("Introduzca un número: ")) #Se declaro la acción a realizar
 
-def numeroImpar(self): #Se declaro el metodo
-    self.__numeroImpar
-    return 0
-
-def validaNumero(self):
-    self.__validaNumero
-    return 0
-
-a = 0
-
-a = int(input("Introduzca un número: ")) #Se declaro la acción a realizar
-if a%2 == 0: #Usamos el Modulo(%2) para saber si el numero es par o impar
-    print("El numero: es par ")
-else:
-    print("El numero:  es impar")
+    if a%2 == 0: #Usamos el Modulo(%2) para saber si el numero es par o impar
+      print("El numero: es par ")
+    else:
+      print("El numero:  es impar")
 
 
 
